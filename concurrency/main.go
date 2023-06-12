95acdebef41aebf15b6d2b446f8936ce2bb1910c
NIRVXSH <setthanan2544@gmail.com>
2023-06-12 10:26:12 +0700

    concurrency final

--- concurrency/main.go
+++ concurrency/main.go
@@ -5,6 +5,21 @@ import (
 	"sync"
 )
 
+type numbers struct {
+	a int
+	b int
+}
+
+type sum struct {
+	r int
+}
+
+func workers(jobCh <-chan numbers, resultsCh chan<- sum) {
+	for job := range jobCh {
+		resultsCh <- sum{job.a + job.b}
+	}
+}
+
 func main() {
 	//Basic -> sync.Waitgroup
 	a := []int{1, 2, 3, 4, 5}
@@ -33,6 +48,56 @@ func main() {
 		}
 	}()
 	wg.Wait()
+	fmt.Println("")
+	fmt.Println("-----------------chan-----------------------")
+	fmt.Println("")
 	//chan
+	numberCh := make(chan int)
+	msgCh := make(chan string)
+	//number
+	go func(numberCh chan int) {
+		numberCh <- 10
+	}(numberCh)
+
+	//msg
+	go func(msgCh chan string) {
+		msgCh <- "Hello NIRVXSH"
+	}(msgCh)
+
+	number := <-numberCh
+	msg := <-msgCh
+
+	fmt.Println(number)
+	fmt.Println(msg)
+
 	//Worker Pool
+	fmt.Println("")
+	fmt.Println("-----------------Worker Pool-----------------------")
+
+	nums := []numbers{
+		{a: 1, b: 2},
+		{a: 1, b: 2},
+		{a: 1, b: 2},
+		{a: 1, b: 2},
+		{a: 1, b: 2},
+	}
+	fmt.Println(nums)
+	jobCh := make(chan numbers, len(nums))
+	resultsCH := make(chan sum, len(nums))
+
+	for _, n := range nums {
+		jobCh <- n
+	}
+	close(jobCh)
+
+	numWorkers := 2
+	for w := 0; w < numWorkers; w++ {
+		go workers(jobCh, resultsCH)
+	}
+	results := make([]sum, 0)
+	for a := 0; a < len(nums); a++ {
+		temp := <-resultsCH
+		results = append(results, temp)
+	}
+	fmt.Println(results)
 }
